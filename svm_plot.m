644424461d520c49d29c177e3fd282eed81dfd95
bill <phongbinh2511@gmail.com>
2020-12-10 15:42:56 +0800

    [master] Pre-allocated train_pos and train_neg.

--- svm_plot.m
+++ svm_plot.m
@@ -1,7 +1,9 @@
 train_data = readmatrix('data/basic_train');
 train_data_N = numel( train_data(:, 1) );
-train_pos = [];
-train_neg = [];
+train_pos = zeros(train_data_N, 2);
+train_pos_N = 0;
+train_neg = zeros(train_data_N, 2);
+train_neg_N = 0;
 
 xmin = inf;
 xmax = -inf;
@@ -13,11 +15,16 @@ for i = 1:train_data_N
     ymin = min(ymin, train_data(i, 2));
     ymax = max(ymax, train_data(i, 2));
     if train_data(i, 3) == 1
-        train_pos(end + 1, :) = train_data(i, 1:2);
+        train_pos_N = train_pos_N + 1;
+        train_pos(train_pos_N, :) = train_data(i, 1:2);
     else
-        train_neg(end + 1, :) = train_data(i, 1:2);
+        train_neg_N = train_neg_N + 1;
+        train_neg(train_neg_N, :) = train_data(i, 1:2);
     end
 end
+train_pos = train_pos(1:train_pos_N, :);
+train_neg = train_neg(1:train_neg_N, :);
+
 xmin = xmin - 1;
 xmax = xmax + 1;
 ymin = ymin - 1;
